6f8460b7152f9b2c8d629a4e89e18582531e050a
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-08 17:18:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,8 +17,11 @@ def find_max_value(array)
   while counter < array.length do
     if array[counter] > max
       max = array[counter]
+      return max
     else
       counter +=
+end
+end
 end
 
 def find_min_value(array)
